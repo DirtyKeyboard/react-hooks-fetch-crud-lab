cb030a0ce55001e2bb6d12acac66b80200376b05
DirtyKeyboard <andrewxx000222@icloud.com>
2023-03-21 17:14:01 -0500

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
-
+  const [questions, setQuestions] = useState([]);
+  useEffect(async() => {
+    const raw = await fetch('http://localhost:4000/questions')
+    const data = await raw.json()
+    setQuestions(data);
+  },[])
+  function deleteQuestion(id)
+  {
+    const newQuestions = questions.filter(el => (el.id !== id))
+    setQuestions(newQuestions)
+  }
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm setQuestions={setQuestions}/> : <QuestionList questions={questions} deleteQuestion={deleteQuestion}/>}
     </main>
   );
 }
--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, deleteQuestion}) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -8,7 +8,12 @@ function QuestionItem({ question }) {
       {answer}
     </option>
   ));
-
+    function handleDeleteClick() {
+      const configObj = {method: 'DELETE'}
+      // console.log(question.id)
+      fetch(`http://localhost:4000/questions/${question.id}`,configObj)
+      .then(() => deleteQuestion(question.id))
+    }
   return (
     <li>
       <h4>Question {id}</h4>
@@ -17,7 +22,7 @@ function QuestionItem({ question }) {
         Correct Answer:
         <select defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDeleteClick}>Delete Question</button>
     </li>
   );
 }
